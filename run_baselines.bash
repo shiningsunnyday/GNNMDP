73812fcd3e55b9ce747e805781459b9149399d93
Michael Sun <michaelsun18@yahoo.com>
2023-03-10 14:22:26 -0800

    minor

--- run_baselines.bash
+++ run_baselines.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 algos='gnn-mdp'
-lambdas='0.03 0.1 0.3 1. 3. 10.0'
+lambdas='3. 10.0'
 for lambda in $lambdas
 do
     echo $1
     echo $lambda
-    python compute_dim.py --algo gnn-mdp --flag $1 --gnn_model distmask --suffix distmask$1 --mask_c $lambda
+    python compute_dim.py --algo gnn-mdp --flag $1 --gnn_model distmask --suffix distmask$1 --mask_c $lambda --num_iters 1
 done
 
 
