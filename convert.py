a37c43535835d5df002a7752d9e97eb5d29e4c0a
Michael Sun <michaelsun18@yahoo.com>
2023-03-30 15:41:28 -0700

    minor

--- convert.py
+++ convert.py
@@ -3,7 +3,8 @@ from tqdm import tqdm
 from models import utils as ut
 
 dirname ='./data/sat_to_mdp/3sat/uf20-91/'
-for fname in tqdm(os.listdir(dirname)):
+filenames = list(filter(lambda x: '_ntable' not in x, os.listdir(dirname)))
+for fname in tqdm(filenames):
     path = dirname+fname.replace(".txt","_ntable.txt")
     if os.path.exists(path): continue
     n,N,M,adj = ut.load_3sat(dirname+fname)
@@ -12,4 +13,4 @@ for fname in tqdm(os.listdir(dirname)):
     f = open(path,"w+")
     for v in ntable.values():
         f.write(' '.join(list(map(str,v)))+'\n')
-    f.close()
+    f.close(d)
